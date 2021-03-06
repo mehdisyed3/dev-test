a43fa78a17cb2a977bf5e76530e01156301b0dfb
Syed Mehdi <mehdisyed3@gmail.com>
2021-03-06 00:26:40 -0500

    Test Complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { addItem, deleteItem } from './redux/actions';
+import './index.css';
 
 function App() {
-	return <div className='app'>hello</div>;
+	const [text, setText] = useState('');
+	const state = useSelector((state) => state);
+	const dispatch = useDispatch();
+
+	const displayWishList = state.wishList.map((wish, i) => (
+		<p onClick={() => dispatch(deleteItem(wish))} key={i}>
+			{wish}
+		</p>
+	));
+
+	// this code block will only allow a wish to be added if the input value is not undefined i.e ""
+	const addToState = (e) => {
+		if (text[0] !== undefined) {
+			dispatch(addItem(text));
+			setText('');
+		}
+	};
+
+	const submitHandler = () => {
+		// console.log("submitting,", state.wishList)
+
+		// this block of code is so that user can't submit an empty wishList
+		if (state.wishList.length > 0) {
+			dispatch({ type: '' });
+			alert('Wish list submitted to Santa!');
+		}
+	};
+
+	return (
+		<div className='app'>
+			<div className='app__container'>
+				<h3>MY WISHLIST</h3>
+				<div className='app__display'>{displayWishList}</div>
+
+				<input
+					className='app__input '
+					type='text'
+					onChange={(e) => setText(e.target.value.trimStart())}
+					value={text}
+				/>
+				<button className='app__buttonOne' onClick={addToState}>
+					Add
+				</button>
+
+				<button className='app__buttonTwo' onClick={submitHandler}>
+					Submit
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -8,16 +8,29 @@ const INITIAL_STATE = {
 const reducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case ADD_ITEM:
+			const newWish = action.payload.trim();
+			const array = !state.wishList.some(
+				(item) => item.toLowerCase() === newWish.toLowerCase()
+			)
+				? [...state.wishList, newWish.trim()]
+				: [...state.wishList];
+
 			return {
-				wishList: '',
+				wishList: array,
 			};
 		case DELETE_ITEM:
+			const updatedArr = state.wishList.filter(
+				(item) => item !== action.payload
+			);
 			return {
-				wishList: '',
+				wishList: updatedArr,
 			};
+		// I would usually choose to return the current state as default case, however test requires me to
+		// clear the wishList upon submitting without creating a new action, hence I chose to return
+		// an empty array as default case
 		default:
 			return {
-				wishList: '',
+				wishList: [],
 			};
 	}
 };
